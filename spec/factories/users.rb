4dda31b35784ea5192bcbd9424dd811a3f0c61c0
Mats Rietdijk <matsrietdijk@gmail.com>
2015-11-12 10:16:39 +0100

    Change comments in user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   # when your factory should be accessable under multiple names
   # you can give it aliases
   factory :user, aliases: [:author, :liker, :commenter] do
-    # static values can be passed without a block
+    # static values can/should be passed without a block
     name 'Username'
     # use `sequence` when data should be unique
     # also; other attributes can be accessed when generating a value
@@ -12,8 +12,8 @@ FactoryGirl.define do
     trait :with_posts do
       # using a callback the currently created object can be passed
       # to functions
-      # default callbacks: after_create, after_build,
-      #                    before_create & before_build
+      # default callbacks: before_create, after_create,
+      #                    after_build & after_stub
       after(:build) do |user|
         user.posts = build_list(:post, 2, author: user)
       end
